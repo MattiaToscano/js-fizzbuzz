c95214e95bbd883b180569065d903bfecd8705f3
MattiaToscano <mattia1997ct@gmail.com>
2025-03-18 13:35:04 +0100

    [UPD] Inseriti commenti

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
-for(let i = 1; i<=100; i++){
-    if(i % 3 ===0 && i % 5 ===0){
+// Inizializzo ciclo for.
+for(let i = 1; i<=100; i++){ 
+    if(i % 3 ===0 && i % 5 ===0){ // Se il numero è divisibile per 3 e 5 stampo FizzBuzz.
         console.log(`FizzBuzz`);  
     }
-    else if(i % 3 ==0){
+    else if(i % 3 ==0){ // Se il numero è divisibile per 3 stampo Fizz.
         console.log(`Fizz`);  
     }
-    else if(i % 5 ==0){
+    else if(i % 5 ==0){ // Se il numero è divisibile per 5 stampo Buzz.
         console.log(`Buzz`); 
     }  
-    else{
+    else{   //Se il numero non è divisibile per 3 e 5 stampo il numero.
         console.log(i);
     }
 }
