4e97d5716a3a803688c9d1792ae1c43da7dcc4a3
MattiaToscano <mattia1997ct@gmail.com>
2025-03-18 13:32:24 +0100

    [UPD] Fine esercizio

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
 for(let i = 1; i<=100; i++){
-    if(i % 3 ==0){
+    if(i % 3 ===0 && i % 5 ===0){
+        console.log(`FIZZBUZZ`);  
+    }
+    else if(i % 3 ==0){
         console.log(`FIZZ`);  
     }
     else if(i % 5 ==0){
-        console.log(`BUZZ`);  
-    }
-    else if(i % 3 ==0 && i % 5 ==0){
-        console.log(`FIZZBUZZ`);  
-    }
+        console.log(`BUZZ`); 
+    }  
     else{
         console.log(i);
     }
-    
-    
-
-
 }
+    
+
+
